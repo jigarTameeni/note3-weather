8c83700f4f16f84fc9e339139928181184af453e
Jigar <brisktar@Brisktars-iMac.local>
2019-05-21 15:51:58 +0530

    init

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+
+const express = require('express')
+
+const app = express()
+
+console.log(__dirname)
+console.log(path.join(__dirname,'../public'))
+
+app.use(express.static(path.join(__dirname,'../public')))
+
+// app.get('/get',(req,res) => {
+
+//     res.send('<h1>Header</h1>')
+        
+
+// })
+
+
+// app.get('/about',(req,res)=>{
+//     res.send('about')
+// })
+
+// app.get('/weather',(req,res)=>{
+//    res.send({
+//        type:'Cel',
+//        value:'val'
+//    })
+// })
+
+app.listen(3000,()=>{
+    console.log("server started")
+})
